2654a643abe266d8bdf59d7ca1355896491f6afa
Dharma Gupta <101480345+d49gupta@users.noreply.github.com>
2024-08-15 15:48:16 -0400

    Update servo_control.cpp to add setHigh and setLow

--- src/servo_control.cpp
+++ src/servo_control.cpp
@@ -1,14 +1,14 @@
 
 #include <GPIO_Control.hpp>
 
-bool ServoMotor::setup() {
-    chip = gpiod_chip_open_by_name(CHIP_NAME);
+bool ServoMotor::setup(std::string chip_name, int pwm_pin) {
+    chip = gpiod_chip_open_by_name(chip_name);
     if (!chip) {
         std::cerr << "Failed to open GPIO chip" << std::endl;
         return false;
     }
 
-    line = gpiod_chip_get_line(chip, PWM_PIN);
+    line = gpiod_chip_get_line(chip, pwm_pin);
     if (!line) {
         std::cerr << "Failed to get GPIO line" << std::endl;
         gpiod_chip_close(chip);
@@ -24,7 +24,7 @@ bool ServoMotor::setup() {
     return true;
 }
 
-void ServoMotor::move(int angle) {
+void ServoMotor::move(int time, int angle) {
     // Ensure the angle is within 0 to 180 degrees
     if (angle < 0) angle = 0;
     if (angle > 180) angle = 180;
@@ -33,11 +33,23 @@ void ServoMotor::move(int angle) {
     int duty_cycle = angle_to_duty_cycle(angle);
 
     // Send the PWM signal
-    for (int i = 0; i < 50; ++i) {
+    for (int i = 0; i < 50*time; ++i) {
         gpiod_line_set_value(line, 1);
         std::this_thread::sleep_for(std::chrono::microseconds(duty_cycle));
         gpiod_line_set_value(line, 0);
         std::this_thread::sleep_for(std::chrono::microseconds(20000 - duty_cycle));
+    }
+}
+
+void ServoMotor::setHigh() {
+    if (gpiod_line_set_value(line, 1) < 0) {
+        throw std::runtime_error("Failed to set GPIO line high");
+    }
+}
+
+void ServoMotor::setLow() {
+    if (gpiod_line_set_value(line, 0) < 0) {
+        throw std::runtime_error("Failed to set GPIO line low");
     }
 }
 
