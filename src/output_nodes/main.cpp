bbcab45b3fd6eeea98e33c982229d6bb28cd2928
Dharma Gupta <dharma.anita@gmail.com>
2024-12-28 16:33:47 -0500

    cleaned up repo

--- src/output_nodes/main.cpp
+++ src/output_nodes/main.cpp
@@ -5,7 +5,7 @@ int main(int argc, char * argv[])
   rclcpp::init(argc, argv);
   auto first_subscriber = std::make_shared<counterSubscriber>("counter_subscriber1", 25, "counter_topic1");
   auto second_subscriber = std::make_shared<counterSubscriber>("counter_subscriber2", 25, "counter_topic2");
-  // auto second_subscriber = std::make_shared<ImuSubscriber>("imu_subscriber", 25, "imu_topic");
+  auto second_subscriber = std::make_shared<ImuSubscriber>("imu_subscriber", 25, "imu_topic");
 
   auto sensor_fusion = std::make_shared<sensorFusion>(first_subscriber, second_subscriber);
   rclcpp::executors::MultiThreadedExecutor executor;
