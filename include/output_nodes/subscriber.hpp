71e54cd7af78c567f46e35ca8b4870b930616c68
Dharma Gupta <dharma.anita@gmail.com>
2024-12-30 01:04:54 -0500

    trying some stuff

--- include/output_nodes/subscriber.hpp
+++ include/output_nodes/subscriber.hpp
@@ -4,8 +4,11 @@
 
 #include "rclcpp/rclcpp.hpp"
 #include "std_msgs/msg/string.hpp"
-#include "std_msgs/msg/float32_multi_array.hpp"
 #include "std_msgs/msg/int32.hpp"
+#include "sensor_msgs/msg/imu.hpp"
+
+#include "geometry_msgs/msg/vector3.hpp"
+#include "geometry_msgs/msg/quaternion.hpp"
 
 class counterSubscriber : public rclcpp::Node
 {
@@ -21,21 +24,26 @@ class ImuSubscriber : public rclcpp::Node
 {
   public:
     ImuSubscriber(std::string nodeName, int size, std::string topicName);
-
+    struct IMUdata {
+      geometry_msgs::msg::Vector3 linear_acceleration;
+      geometry_msgs::msg::Vector3 angular_velocity;
+      geometry_msgs::msg::Quaternion orientation;
+    };
+    
+    dataCache<IMUdata> imuCache;
   private:
-    rclcpp::Subscription<std_msgs::msg::Float32MultiArray>::SharedPtr subscription_;
-    dataCache<int> imuCache;
+    rclcpp::Subscription<sensor_msgs::msg::Imu>::SharedPtr subscription_;
 };
 
 class sensorFusion : public rclcpp::Node {
 public:
     sensorFusion(const std::shared_ptr<counterSubscriber> counterSensor1, 
-                 const std::shared_ptr<counterSubscriber> counterSensor2);
+                 const std::shared_ptr<ImuSubscriber> counterSensor2);
 
     void fuseData();
 private:
     const std::shared_ptr<counterSubscriber> counterSensor1; 
-    const std::shared_ptr<counterSubscriber> counterSensor2;
+    const std::shared_ptr<ImuSubscriber> counterSensor2;
     rclcpp::TimerBase::SharedPtr timer_;
  
 };
--- src/output_nodes/subscriber.cpp
+++ src/output_nodes/subscriber.cpp
@@ -11,22 +11,24 @@ counterSubscriber::counterSubscriber(std::string nodeName, int size, std::string
 
 void sensorFusion::fuseData() {
   int sensor1 = counterSensor1->counterCache.newestValue();
-  int sensor2 = counterSensor2->counterCache.newestValue();
-
-  std::cout << "Sum is: " << (sensor1 + sensor2) << std::endl; //add mutexes for shared resource (counter cache of each sensor)
+  ImuSubscriber::IMUdata sensor2 = counterSensor2->imuCache.newestValue();
 }
 
-ImuSubscriber::ImuSubscriber(std::string nodeName, int size, std::string topicName) : Node(nodeName), imuCache(size) { //use sensor_msgs imu and make imuCache
+ImuSubscriber::ImuSubscriber(std::string nodeName, int size, std::string topicName) : Node(nodeName), imuCache(size) {
   auto topic_callback =
-  [this](std_msgs::msg::Float32MultiArray::UniquePtr msg) -> void {
-    std::vector<float> data = msg->data;
-    RCLCPP_INFO(this->get_logger(), "Accelerometer Readings: X: %f, Y: %f, Z: %f", data[0], data[1], data[2]);
-    RCLCPP_INFO(this->get_logger(), "Gyroscope Readings: X: %f, Y: %f, Z: %f", data[3], data[4], data[5]);
+  [this, topic = topicName](sensor_msgs::msg::Imu::UniquePtr msg) -> void {
+    IMUdata imu_data;
+    imu_data.linear_acceleration = msg -> linear_acceleration;
+    imu_data.angular_velocity = msg->angular_velocity;
+    imu_data.orientation = msg->orientation;
+    this->imuCache.enqueue(imu_data);
+
+    RCLCPP_INFO(this->get_logger(), "IMU data received on topic %s", topic.c_str());
   };
-  subscription_ = this->create_subscription<std_msgs::msg::Float32MultiArray>(topicName, 10, topic_callback);
+  subscription_ = this->create_subscription<sensor_msgs::msg::Imu>(topicName, 10, topic_callback);
 }
 
-sensorFusion::sensorFusion(const std::shared_ptr<counterSubscriber> counterSensor1, const std::shared_ptr<counterSubscriber> counterSensor2)  
+sensorFusion::sensorFusion(const std::shared_ptr<counterSubscriber> counterSensor1, const std::shared_ptr<ImuSubscriber> counterSensor2)  
                         : Node("SensorFusion"), counterSensor1(counterSensor1), counterSensor2(counterSensor2) {
 
   timer_ = this->create_wall_timer(std::chrono::milliseconds(100), std::bind(&sensorFusion::fuseData, this)); 
